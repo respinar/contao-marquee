9f4484fa81fc71fb7e02d10d8bced76f3fea58eb
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2015-12-22 11:33:24 +0330

    add new parameters

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -12,7 +12,7 @@
 /**
  * Add palettes to tl_module
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['marquee']    = '{title_legend},name,headline,type;{marqee_legend},marquee,numberOfItems;{config_legend},marquee_speed,marquee_padding,marquee_hover,marquee_sibling;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['marquee']    = '{title_legend},name,headline,type;{marqee_legend},marquee,numberOfItems;{config_legend},marquee_speed,marquee_direction,marquee_delayBeforeStart,marquee_gap,marquee_pauseOnHover,marquee_duplicated;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 
 
 /**
@@ -37,31 +37,54 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['marquee_speed'] = array
     'sql'                  => "int(10) NOT NULL default '200'"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_padding'] = array
+$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_delayBeforeStart'] = array
 (
-	'label'                => &$GLOBALS['TL_LANG']['tl_module']['marquee_padding'],
+	'label'                => &$GLOBALS['TL_LANG']['tl_module']['marquee_delayBeforeStart'],
+	'exclude'              => true,
+	'inputType'            => 'text',
+	'eval'                 => array('tl_class'=>'w50'),
+    'sql'                  => "int(10) NOT NULL default '0'"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_gap'] = array
+(
+	'label'                => &$GLOBALS['TL_LANG']['tl_module']['marquee_gap'],
 	'exclude'              => true,
 	'inputType'            => 'text',
 	'eval'                 => array('tl_class'=>'w50'),
     'sql'                  => "int(10) NULL"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_hover'] = array
+
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_pauseOnHover'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['marquee_hover'],
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['marquee_pauseOnHover'],
 	'exclude'                 => true,
 	'inputType'               => 'checkbox',
 	'eval'                    => array('tl_class'=>'w50 m12'),
 	'sql'                     => "char(1) NOT NULL default ''"
 );
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_sibling'] = array
+$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_duplicated'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['marquee_sinbling'],
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['marquee_duplicated'],
 	'exclude'                 => true,
 	'inputType'               => 'checkbox',
 	'eval'                    => array('tl_class'=>'w50 m12'),
 	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['marquee_direction'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['marquee_direction'],
+	'default'                 => 'right',
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('right', 'left', 'up','down'),
+	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
+	'eval'                    => array('tl_class'=>'w50'),
+	'sql'                     => "varchar(16) NOT NULL default ''"
 );
 
 
