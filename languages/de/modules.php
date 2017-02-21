e5956f18be5552b11eed381cdc3c13867dac82ee
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2017-02-21 16:51:32 +0330

    Add germany lanugaes

---
+++ languages/de/modules.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Marquee
+ * @author    Hamid Abbaszadeh info@respinar.com
+ * @license   LGPL-3.0+
+ * @copyright respinar 2015
+ */
+
+
+/**
+ * Back end modules
+ */
+$GLOBALS['TL_LANG']['MOD']['marquee'] = array('Laufschrift', 'Verwaltung von Laufschrifttexten.');
+
+
+/**
+ * Front end modules
+ */
+$GLOBALS['TL_LANG']['FMD']['marquee'] = array('Laufschrift', 'Eine Laufschrift hinzufügen');
---
+++ languages/de/tl_marquee.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @package   Marquee
+ * @author    Hamid Abbaszadeh info@respinar.com
+ * @license   LGPL-3.0+
+ * @copyright respinar 2015
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_marquee']['title']     = array('Titel', 'Bitte geben die den Titel der Laufschrift ein.');
+$GLOBALS['TL_LANG']['tl_marquee']['protected'] = array('Lufschrift schützen','Die Laufschrift nur bestimmten Mitgliedergruppen zeigen');
+$GLOBALS['TL_LANG']['tl_marquee']['groups']    = array('Mitgliedergruppen Rechte','Nur diese Mitgliedergruppen werden die Elemente der Laufschrift sehen können.');
+
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_marquee']['version']       = "Laufschrift Module";
+$GLOBALS['TL_LANG']['tl_marquee']['deleteConfirm'] = 'Beim Löschen einer Laufschrift, werden auch die Texte mit gelöscht! Wollen sie die Laufschrift mit der ID %s wirklich löschen?';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_marquee']['new']    		= array('Neue Laufschrift', 'Create a new Laufschrift');
+$GLOBALS['TL_LANG']['tl_marquee']['edit']   		= array('Laufschrift bearbeiten', 'Laufschrift ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_marquee']['editheader']     = array('Laufschrift-Eigenschaften bearbeiten', 'Eigenschaften der Laufschrift ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_marquee']['copy']   		= array('Laufschrift kopieren', 'Laufschrift ID %s kopieren');
+$GLOBALS['TL_LANG']['tl_marquee']['delete'] 		= array('Laufschrift löschen', 'Laufschrift ID %s löschen');
+$GLOBALS['TL_LANG']['tl_marquee']['show']   		= array('Laufschrift details', 'Details der Laufschrift ID %s');
+ 
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_marquee']['title_legend']     = 'Laufschrift Titel';
+$GLOBALS['TL_LANG']['tl_marquee']['protected_legend'] = 'Zugriffsschutz';
