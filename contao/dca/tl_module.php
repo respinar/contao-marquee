1a649a50809bf7f6356abbb095516a39649e8df6
Hamid Peywasti <hamid@respinar.com>
2024-01-25 17:10:14 +0330

    Update License

--- contao/dca/tl_module.php
+++ contao/dca/tl_module.php
@@ -1,11 +1,11 @@
 <?php
 
-/**
- * Contao Open Source CMS
+/*
+ * This file is part of Contao Marquee Bundle.
  *
- * Copyright (c) 2005-2017 Leo Feyer
+ * (c) Hamid Peywasti 2015-2024 <hamid@respinar.com>
  *
- * @license LGPL-3.0+
+ * @license LGPL-3.0-or-later
  */
 
 
